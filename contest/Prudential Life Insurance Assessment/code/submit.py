76c2a4207feedc778fe41e0b0032b6b8ac36fec2
luqi03 <luqi03@baidu.com>
2015-12-12 20:55:12 +0800

    update

--- contest/Prudential Life Insurance Assessment/code/submit.py
+++ contest/Prudential Life Insurance Assessment/code/submit.py
@@ -4,11 +4,11 @@ from sklearn.preprocessing import LabelEncoder
 import xgboost as xgb
 
 
-FOLDER = './data/'
+FOLDER = '../data/'
 FILE1 = 'train.csv'
 FILE2 = 'test.csv'
-FILE3 = 'train_numeric_features.csv'
-FILE4 = 'test_numeric_features.csv'
+FILE3 = 'train3.csv'
+FILE4 = 'test3.csv'
 
 
 class BuildXgb():
@@ -28,8 +28,9 @@ class BuildXgb():
     def _feature_select(self, df_train, df_test):
 
 	# Drop four features
-        return df_train.drop(['T1_V10', 'T1_V13', 'T2_V7', 'T2_V10'], axis=1), \
-               df_test.drop(['T1_V10', 'T1_V13', 'T2_V7', 'T2_V10'], axis=1)
+        # return df_train.drop(['T1_V10', 'T1_V13', 'T2_V7', 'T2_V10'], axis=1), \
+        #       df_test.drop(['T1_V10', 'T1_V13', 'T2_V7', 'T2_V10'], axis=1)
+        return df_train, df_test
 
     def _data_clean_factor(self, df_train, df_test):
 
@@ -58,7 +59,7 @@ class BuildXgb():
         df_train_cleaned = pd.DataFrame(df[df['Train_Set'] == 1])
         df_test_cleaned = pd.DataFrame(df[df['Train_Set'] == 0])
         df_train_cleaned.drop(['Train_Set'], axis=1, inplace=True)
-        df_test_cleaned.drop(['Hazard', 'Train_Set'], axis=1, inplace=True)
+        df_test_cleaned.drop(['Response', 'Train_Set'], axis=1, inplace=True)
         return df_train_cleaned.astype(float), df_test_cleaned.astype(float)
 
     def _data_clean_all_ohe(self, df_train, df_test):
@@ -70,21 +71,21 @@ class BuildXgb():
         df.reset_index(inplace=True)
         df.drop(['index'], axis=1, inplace=True)
         for col_name in df.columns.tolist():
-            if col_name != 'Train_Set' and col_name != 'Id' and col_name != 'Hazard' and col_name != 'T2_V1' and col_name != 'T2_V2':
+            if col_name != 'Train_Set' and col_name != 'Id' and col_name != 'Response':
                 df = pd.concat([df, pd.get_dummies(df[col_name], prefix=col_name)], axis=1)
                 df.drop([col_name], axis=1, inplace=True)
         # Split the train and test again
         df_train_cleaned = pd.DataFrame(df[df['Train_Set'] == 1])
         df_test_cleaned = pd.DataFrame(df[df['Train_Set'] == 0])
         df_train_cleaned.drop(['Train_Set'], axis=1, inplace=True)
-        df_test_cleaned.drop(['Hazard', 'Train_Set'], axis=1, inplace=True)
+        df_test_cleaned.drop(['Response', 'Train_Set'], axis=1, inplace=True)
         return df_train_cleaned.astype(float), df_test_cleaned.astype(float)
 
     def _data_clean_all_factor(self, df_train, df_test):
 
 	# Convert all features to factors
         for col_name in df_train.columns.tolist():
-            if col_name != 'Id' and col_name != 'Hazard':
+            if col_name != 'Id' and col_name != 'Response':
                 le = LabelEncoder()
                 le.fit(list(df_train[col_name]) + list(df_test[col_name]))
                 df_train.loc[:, col_name] = le.transform(df_train[col_name])
@@ -115,25 +116,25 @@ class BuildXgb():
 
         # model training
         if self.y_transform == 'log':
-            y = np.log(self._train['Hazard'].values)
+            y = np.log(self._train['Response'].values)
         elif self.y_transform == 'sqrt':
-            y = np.sqrt(self._train['Hazard'].values)
+            y = np.sqrt(self._train['Response'].values)
         else:
-            y = self._train['Hazard'].values
+            y = self._train['Response'].values
 
-        X = self._train.drop(['Id', 'Hazard'], axis=1).values
+        X = self._train.drop(['Id', 'Response'], axis=1).values
 
         plst = self.xgb_params[0]
         num_rounds = self.xgb_params[1]
 
-        train_xgb = xgb.DMatrix(X, label=y)
+        train_xgb = xgb.DMatrix(X, label=y, missing=np.NaN)
         self._rgr = xgb.train(plst, train_xgb, num_rounds)
 
     def _predict(self):
 
         # predict for test set
         X_test = self._test.drop(['Id'], axis=1).values
-        test_xgb = xgb.DMatrix(X_test)
+        test_xgb = xgb.DMatrix(X_test, missing=np.NaN)
 
         if self.y_transform == 'log':
             y_predict = np.exp(self._rgr.predict(test_xgb))
@@ -144,7 +145,7 @@ class BuildXgb():
 
         # write to file
         test_id = self._test['Id'].astype(int).values
-        df_result = pd.DataFrame({'Id': test_id, 'Hazard': y_predict})
+        df_result = pd.DataFrame({'Id': test_id, 'Response': y_predict})
         df_result.set_index('Id', inplace=True)
         df_result.to_csv(self.folder+'Model{}.csv'.format(self.model_idx))
 
@@ -175,13 +176,15 @@ def main():
     df_test_numeric = pd.read_csv(FOLDER+FILE4)
 
     # Build Model 1
+    print 'Start build Model 1!'
     params = {'objective': 'reg:linear',
-              'eta': 0.005,
-              'subsample': 0.7,
-              'max_depth': 9,
-              'min_child_weight': 6,
-              'colsample_bytree': 0.7,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 1000
     bx = BuildXgb(model_idx=1, y_transform=None, feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -190,13 +193,13 @@ def main():
 
     # Build Model 2
     params = {'objective': 'count:poisson',
-              'eta': 0.005,
-              'subsample': 0.9,
-              'max_depth': 6,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=2, y_transform=None, feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -205,13 +208,13 @@ def main():
 
     # Build Model 3
     params = {'objective': 'count:poisson',
-              'eta': 0.005,
-              'subsample': 0.7,
-              'max_depth': 6,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=3, y_transform=None, feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -220,13 +223,13 @@ def main():
 
     # Build Model 4
     params = {'objective': 'reg:linear',
-              'eta': 0.005,
-              'subsample': 0.9,
-              'max_depth': 6,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=4, y_transform='log', feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -235,13 +238,13 @@ def main():
 
     # Build Model 5
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.7,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=5, y_transform='log', feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -250,13 +253,13 @@ def main():
 
     # Build Model 6
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.9,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=6, y_transform='sqrt', feature_transform='factor', xgb_params=[list(params.items()), num_rounds])
@@ -265,13 +268,13 @@ def main():
 
     # Build Model 7
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.9,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=7, y_transform='sqrt', feature_transform='ohe', xgb_params=[list(params.items()), num_rounds])
@@ -279,14 +282,15 @@ def main():
     print 'Model 7 has been built!'
 
     # Build Model 8
+    '''
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.9,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=8, y_transform='sqrt', feature_transform='all_ohe', xgb_params=[list(params.items()), num_rounds])
@@ -295,13 +299,13 @@ def main():
 
     # Build Model 9
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.9,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=9, y_transform='sqrt', feature_transform='all_factor', xgb_params=[list(params.items()), num_rounds])
@@ -310,33 +314,33 @@ def main():
 
     # Build Model 10
     params = {'objective': 'reg:linear',
-              'eta': 0.003,
-              'subsample': 0.9,
-              'max_depth': 9,
-              'min_child_weight': 1,
-              'colsample_bytree': 0.5,
-              'gamma': 5,
-              'silent': 1
+              'eta': 0.02,
+              'subsample': 0.8,
+              'max_depth': 11,
+              'min_child_weight': 3,
+              'colsample_bytree': 0.65,
+              'silent': 1,
+              'nthread': 10
               }
     num_rounds = 5000
     bx = BuildXgb(model_idx=10, y_transform='sqrt', feature_transform='numeric', xgb_params=[list(params.items()), num_rounds])
     bx.build(df_train_numeric, df_test_numeric)
     print 'Model 10 has been built!'
-
+    '''
     # Define Weights
-    w = [1, 1, 1, 1, 1, 3, 3, 3, 4, 2]
+    w = [1, 1, 1, 1, 1, 3, 3]
 
     # Ensemble results
     y_list = []
-    for model_idx in range(1, 11):
+    for model_idx in range(1, 8):
         df = pd.read_csv(FOLDER+'Model{}.csv'.format(model_idx))
-        y_list.append(df['Hazard'].values)
+        y_list.append(df['Response'].values)
     y_predict = ensemble_results(y_list, w)
     print "\nEnsemble Completed!\n"
 
     # Prepare submission file
     test_id = df_test['Id'].astype(int).values
-    df_result = pd.DataFrame({'Id': test_id, 'Hazard': y_predict})
+    df_result = pd.DataFrame({'Id': test_id, 'Response': y_predict})
     df_result.set_index('Id', inplace=True)
     df_result.to_csv(FOLDER+'submission.csv')
 
