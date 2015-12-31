739fd5ae5a9f49c705b6b4398cac3d65b08587ce
luqi03 <luqi03@baidu.com>
2015-12-31 22:57:43 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-rm answer.txt
-wget ftp://st01-spi-pubec2.st01.baidu.com/home/spider/luqi03/nowcoder/code/answer.txt
+rm final.txt
+wget ftp://st01-spi-pubec2.st01.baidu.com/home/spider/luqi03/nowcoder/final/ftrl_proximal_lr/final.txt
 git add .
 git commit -am 'update'
 git push
